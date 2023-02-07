7db3dab0323ae057ac5b1aec6e8221643247e515
MyTestCoding34 <romal_game@mail.ru>
2023-02-07 23:56:05 +0600

    create template MVVM

---
+++ app/src/main/java/ru/appromal/calculatorapk/presentation/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.appromal.calculatorapk.presentation
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+}
---
+++ app/src/main/java/ru/appromal/calculatorapk/presentation/MainViewModelFactory.kt
@@ -0,0 +1,6 @@
+package ru.appromal.calculatorapk.presentation
+
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModelFactory:ViewModelProvider.Factory {
+}
